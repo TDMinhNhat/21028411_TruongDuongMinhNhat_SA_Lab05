a2aaf11a2ab774e08e53fcb8c31a592adecd95a2
Trương Dương Minh Nhật <tdminhnhat13092003@gmail.com>
2024-03-13 22:39:24 +0700

    Create base project

---
+++ src/main/java/dev/skyherobrine/sa_lab05/SaLab05Application.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.sa_lab05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaLab05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaLab05Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/skyherobrine/sa_lab05/SaLab05ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.sa_lab05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaLab05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
